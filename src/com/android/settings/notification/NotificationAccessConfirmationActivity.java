11ca2faeaf94abc35843a51789d4056f11e5b1d5
Matías Hernández <matiashe@google.com>
2025-01-03 13:38:10 +0100

    Remove incorrect call to Window.addFlags with a system flag
    The same flag is being (correctly) added in onCreate, so on top of being wrong this was also completely unnecessary.
    
    Bug: 368579654
    Test: manual
    Flag: EXEMPT bugfix
    
    (cherry picked from commit e0510a62eba04140e459cdd8232f35087aa3073b)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4f4d00f42271df5a2727cac373412dd0114076cf)
    Merged-In: I539fca77a1d136bdd0e4892993020884430b8a4e
    Change-Id: I539fca77a1d136bdd0e4892993020884430b8a4e

--- src/com/android/settings/notification/NotificationAccessConfirmationActivity.java
+++ src/com/android/settings/notification/NotificationAccessConfirmationActivity.java
@@ -41,7 +41,6 @@ import android.os.UserHandle;
 import android.service.notification.NotificationListenerService;
 import android.text.TextUtils;
 import android.util.Slog;
-import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 
 import com.android.internal.app.AlertActivity;
@@ -137,20 +136,6 @@ public class NotificationAccessConfirmationActivity extends Activity
         AlertController
                 .create(this, this, getWindow())
                 .installContent(p);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        getWindow().addFlags(
-                WindowManager.LayoutParams.PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
-    }
-
-    @Override
-    public void onPause() {
-        getWindow().clearFlags(
-                WindowManager.LayoutParams.PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
-        super.onPause();
     }
 
     private void onAllow() {
